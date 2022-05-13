73e3cff55e3734150235d85efcd4623a2663b218
ISkvor <starlingy608@gmail.com>
2022-05-13 14:37:23 +0300

    Initial Commit

---
+++ SwiftUIContactList/SwiftUIContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContactListApp.swift
+//  SwiftUIContactList
+//
+//  Created by Юрий Скворцов on 13.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
